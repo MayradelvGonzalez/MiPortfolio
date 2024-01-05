5cd2b847cfe66a612a6292469e8f829d1732c12f
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-05 00:42:10 -0300

    Carpetas

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import logo from '../src/assets/gifLogoN.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <section class="triangle"></section>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
