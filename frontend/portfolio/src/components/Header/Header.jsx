af0670d320d55feac169f9592134697a29f72c2b
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-06 11:53:38 -0300

    acomodandoElementos

--- frontend/portfolio/src/components/Header/Header.jsx
+++ frontend/portfolio/src/components/Header/Header.jsx
@@ -1,9 +1,19 @@
-// import 'header.css';
+import './Header.css'
 function Header() {
     return (
         <div id="header">
-            <h1 className="contacto">Hola soy el header de contacto</h1>
-        </div>)
+           
+                <h2 className="contacto">Hola soy el header de contacto</h2>
+                <ul className="lista">
+                    <li>Telefono</li>
+                    <li>Linkedin</li>
+                    <li>Github</li>
+                    <li>Email</li>
+                </ul>
+            </div>
+
+
+    )
 }
 
 export default Header;
