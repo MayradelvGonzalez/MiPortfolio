e04b48b6b0ed4996e5591eb6c98c8671589d484a
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-18 23:58:19 -0300

    agregMediaQ

--- frontend/portfolio/src/components/Header/Header.jsx
+++ frontend/portfolio/src/components/Header/Header.jsx
@@ -4,12 +4,12 @@ function Header() {
     return (
         <div id="header">
 
-            <div className='logoName'> <img src={LogoNsf} className="App-logo" alt="logo" /><h2 className="contacto">Aun no se que poner aqui</h2></div>
+            <div className='logoName'> <img src={LogoNsf} className="App-logo" alt="logo" /><h2 className="contacto"></h2></div>
             <div> <ul className="lista">
-                <li><a href="tel:+543815160008">Telefono</a></li>
-                <li><a href="https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/">Linkedin</a></li>
-                <li><a href="https://github.com/MayradelvGonzalez">Github</a></li>
-                <li><a href="mailto:mayradelvallegonzalez@gmail.com">Email</a>
+                <li><a href="tel:+543815160008" target='_BLANK'  rel="noopener noreferrer">Telefono</a></li>
+                <li><a href="https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/" target='_BLANK'  rel="noopener noreferrer">Linkedin</a></li>
+                <li><a href="https://github.com/MayradelvGonzalez" target='_BLANK'  rel="noopener noreferrer">Github</a></li>
+                <li><a href="mailto:mayradelvallegonzalez@gmail.com" target='_BLANK'  rel="noopener noreferrer">Email</a>
                     <span class="border border-top"></span>
                     <span class="border border-right"></span>
                     <span class="border border-bottom"></span>
