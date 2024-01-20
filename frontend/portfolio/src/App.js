9a5b5eaa745899deb761e0abdd0bd0acc5096d4d
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-19 21:53:06 -0300

    agregaInfo y Acces

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,33 +1,64 @@
-import LogoB from './assets/gifLogoB.gif';
+import LogoB from './assets/logoGifGris.gif';
 import './App.css';
+import * as React from 'react';
+import { useState } from 'react';
+import Accordion from '@mui/material/Accordion';
+import AccordionActions from '@mui/material/AccordionActions';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Button from '@mui/material/Button';
+
 
 function App() {
+  const [expandedPanel, setExpandedPanel] = useState(null);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpandedPanel(isExpanded ? panel : null);
+  };
   return (
-   
-    <div className="App" role='main'>  
-    
-      <div className="App-header" >
-       
-       
+
+    <div className="App" role='main'>
+
+      <div className="App-header">
+
+
         <h1>
           Mayra del Valle González
         </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          tabIndex={1}
-          aria-label='Link to more info'
+
+        <div>
+        <Accordion
+        expanded={expandedPanel === 'panel1'}
+        onChange={handleChange('panel1')}
+        aria-labelledby="panel1-header"
+        role="region"
+        aria-controls="panel1-content"
+      >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon tabIndex={0} />}
+          aria-expanded={expandedPanel === 'panel1'}
+          id="panel1-header"
         >
-          Full-Stack Developer
-        </a> 
+          <h3>Full-Stack Developer</h3>
+        </AccordionSummary>
+        <AccordionDetails className='panelInfo' id="panel1-content" aria-describedby="panel1-header">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+
+
+
+
+
+        </div>
         <img src={LogoB} className="App-logo" alt="logo de la página" aria-label='Logo de la página' />
-     <div tabIndex={1} aria-label='about me'>lordfkbsdfjkszbfjkzsdbfjkzdfbsdjkfbdsjkfbdjksafbsdkjfb
-    
-     </div>
+        <div aria-label='about me'>lordfkbsdfjkszbfjkzsdbfjkzdfbsdjkfbdsjkfbdjksafbsdkjfb
+
+        </div>
       </div>
-    
+
     </div>
   );
 }
--- frontend/portfolio/src/components/Header/Header.jsx
+++ frontend/portfolio/src/components/Header/Header.jsx
@@ -6,10 +6,10 @@ function Header() {
 
             <div className='logoName' role='navigation'> <img src={LogoNsf} className="App-logo" alt="logo" /></div>
             <div> <ul className="lista">
-                <li><a href="tel:+543815160008" target='_BLANK'  rel="noopener noreferrer" tabIndex={1}>Telefono</a></li>
-                <li><a href="https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/" target='_BLANK'  rel="noopener noreferrer" tabIndex={1}>Linkedin</a></li>
-                <li><a href="https://github.com/MayradelvGonzalez" target='_BLANK'  rel="noopener noreferrer" tabIndex={1}>Github</a></li>
-                <li><a href="mailto:mayradelvallegonzalez@gmail.com" target='_BLANK'  rel="noopener noreferrer" tabIndex={1}>Email</a>
+                <li><a href="tel:+543815160008" target='_BLANK'  rel="noopener noreferrer" tabIndex={0}>Telefono</a></li>
+                <li><a href="https://www.linkedin.com/in/mayra-gonz%C3%A1lez-v/" target='_BLANK'  rel="noopener noreferrer" tabIndex={0}>Linkedin</a></li>
+                <li><a href="https://github.com/MayradelvGonzalez" target='_BLANK'  rel="noopener noreferrer" tabIndex={0}>Github</a></li>
+                <li><a href="mailto:mayradelvallegonzalez@gmail.com" target='_BLANK'  rel="noopener noreferrer" tabIndex={0}>Email</a>
                     <span className="border border-top"></span>
                     <span className="border border-right"></span>
                     <span className="border border-bottom"></span>
