9f046c9e940704a40a828830990fcfbc8cfbf102
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2024-01-21 20:21:08 -0300

    agregElementos

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -19,48 +19,35 @@ function App() {
   return (
 
     <div className="App" role='main'>
-
       <div className="App-header">
-
-
         <h1>
           Mayra del Valle González
         </h1>
-
-        <div>
-        <Accordion
-        expanded={expandedPanel === 'panel1'}
-        onChange={handleChange('panel1')}
-        aria-labelledby="panel1-header"
-        role="region"
-        aria-controls="panel1-content"
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon tabIndex={0} />}
-          aria-expanded={expandedPanel === 'panel1'}
-          id="panel1-header"
-        >
-          <h3>Full-Stack Developer</h3>
-        </AccordionSummary>
-        <AccordionDetails className='panelInfo' id="panel1-content" aria-describedby="panel1-header">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-      </Accordion>
-
-
-
-
-
+        <div className='InfoTitulo'>
+          <Accordion
+            expanded={expandedPanel === 'panel1'}
+            onChange={handleChange('panel1')}
+            aria-labelledby="panel1-header"
+            role="region"
+            aria-controls="panel1-content"
+           
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon tabIndex={0} />}
+              aria-expanded={expandedPanel === 'panel1'}
+              id="panel1-header"
+            >
+              <h3>Full-Stack Developer</h3>
+            </AccordionSummary>
+            <AccordionDetails className='panelInfo' id="panel1-content" aria-describedby="panel1-header">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+              malesuada lacus ex, sit amet blandit leo lobortis eget.
+            </AccordionDetails>
+          </Accordion>
         </div>
         <img src={LogoB} className="App-logo" alt="logo de la página" aria-label='Logo de la página' />
-        <div aria-label='about me'>lordfkbsdfjkszbfjkzsdbfjkzdfbsdjkfbdsjkfbdjksafbsdkjfb
-
-        </div>
       </div>
-
     </div>
   );
 }
-
 export default App;
